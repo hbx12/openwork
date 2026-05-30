777ffa595c3f565b32860a88edf3c6845eceefcc
HBX <107771255+hbx12@users.noreply.github.com>
2026-05-30 19:35:53 +0300

    feat(i18n): export Arabic locale

--- apps/app/src/i18n/locales/index.ts
+++ apps/app/src/i18n/locales/index.ts
@@ -2,6 +2,7 @@
  * Re-export all translation files for convenience
  */
 export { default as en } from "./en";
+export { default as ar } from "./ar";
 export { default as ja } from "./ja";
 export { default as zh } from "./zh";
 export { default as vi } from "./vi";
